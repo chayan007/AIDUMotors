52f482d1965779e7ff94f54bc9e0ea925181e702
Chayan Datta <sonicxxx7@gmail.com>
2019-06-27 23:46:15 +0530

    Public resources add

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,22 @@
+<head>
+    <title>AIDU Motor | @yield('title')</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="description" content="Aidu Motors is the official Chevrolet dealer in Mizoram.">
+    <meta name="keywords" content="Aidu Aizawl Chevrolet Motor Car Sale Mizoram">
+    <meta name="author" content="Chayan Datta">
+    <meta name="MobileOptimized" content="320">
+    <!--Srart Style -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/fonts.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/ion.rangeSlider.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/ion.rangeSlider.skinFlat.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('js/plugin/slick/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('js/plugin/slick/slick-theme.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('js/plugin/magnific/magnific-popup.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('js/plugin/nice_select/nice-select.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <!-- Favicon Link -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+</head>
