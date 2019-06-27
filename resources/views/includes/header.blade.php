35f396f058d6e013782c637c80f6e64ca75fa4ee
Chayan Datta <sonicxxx7@gmail.com>
2019-06-28 00:09:59 +0530

    Nav done

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,70 @@
+
+<!------ Header Start ------>
+<div class="impl_header_wrapper impl_home_wrapper">
+    <div class="impl_top_header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="impl_top_info">
+                        <ul class="impl_header_social">
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        </ul>
+                        <p class="impl_header_time"><i class="fa fa-clock-o" aria-hidden="true"></i> Working Hours - 9 AM To 8 PM <span>sunday closed</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--menu start-->
+    <div class="impl_menu_wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="impl_logo">
+                        <a href="/"><img src="{{ asset('images/logo1.png') }}" alt="Logo" class="img-fluid"></a>
+                    </div>
+                    <button class="impl_menu_btn">
+                        <i class="fa fa-car" aria-hidden="true"></i>
+                        <i class="fa fa-bars" aria-hidden="true"></i>
+                    </button>
+                    <div class="impl_menu_inner">
+                        <div class="impl_logo_responsive">
+                            <a href="/"><img src="{{ asset('images/logo1.png') }}" alt="Logo" class="img-fluid"></a>
+                        </div>
+                        <div class="impl_menu">
+                            <nav>
+                                <div class="menu_cross">
+                                    <i class="fa fa-times" aria-hidden="true"></i>
+                                </div>
+                                <ul>
+                                    <li><a href="/">Home</a></li>
+                                    <li><a href="/about">Company</a></li>
+                                    <li class="dropdown"><a href="javascript:;">Purchase</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="/cars">Car</a></li>
+                                            <li><a href="/accessories">Accessories</a></li>
+                                            <li><a href="/modifications">Modification</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="/services">Services</a></li>
+                                    <li class="dropdown"><a href="javascript:;">Media & Events</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="/media">Media Highlights</a></li>
+                                            <li><a href="/events">Events</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="/gallery">Gallery</a></li>
+                                    <li><a href="/reviews">Reviews</a></li>
+                                    <li><a href="/contact">Contact us</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('public');
+    return view('layouts.public');
 });
 
 Auth::routes();
