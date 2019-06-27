2bb62ae6057504abe27a80a1cc51ccdaf87bf2ef
Chayan Datta <sonicxxx7@gmail.com>
2019-06-28 00:36:54 +0530

    Slider done

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,87 @@
+<!------ Footer Section Start ------>
+<div class="impl_footer_wrapper impl_home_footer">
+    <div class="impl_foo_wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="impl_foo_box footer_abt_text">
+                        <h1 class="impl_foo_title">contact us</h1>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable by injected humour.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="impl_foo_box">
+                        <h1 class="impl_foo_title">Basic Information</h1>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Changing Oil</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Saving Fuel</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Antilock Brakes</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Battery</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Tips On Long Car Trips</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Air Pressure</a></li>
+                            <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Tire Replacement</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="impl_foo_box">
+                        <h1 class="impl_foo_title">contact us</h1>
+                        <ul>
+                            <li>
+                                <div class="impl_foo_icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                                <div class="impl_foo_text">
+                                    <p>+1-202-555-0137</p>
+                                    <p>+1-202-555-0137</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="impl_foo_icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                                <div class="impl_foo_text">
+                                    <p>514 S. Magnolia St.<br>  Orlando , US</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="impl_foo_icon"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+                                <div class="impl_foo_text">
+                                    <a href="#">dummymail@mail.com</a>
+                                    <a href="#">yourmail@mail.com</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="impl_foo_box">
+                        <h1 class="impl_foo_title">Subscribe </h1>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+                        <div class="impl_footer_subs">
+                            <input type="text" class="form-control" placeholder="Enter Your Email">
+                            <button class="foo_subs_btn"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="impl_social_wrapper">
+            <ul>
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+        <div class="impl_btm_footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12">
+                        <p class="impl_copyright">Copyright &copy; 2019 Aidu Motors, Aizawl. Developed by <a href="http://www.thediscretelabs.com" style="font-weight: bolder">The Discrete Labs</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('layouts.public');
+    return view('welcome');
 });
 
 Auth::routes();
