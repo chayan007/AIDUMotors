726ffb1369980b97c03699156f3f59ddee12c9c4
Chayan Datta <sonicxxx7@gmail.com>
2019-06-28 18:52:16 +0530

    Accessory done

--- app/Http/Controllers/AccessoryController.php
+++ app/Http/Controllers/AccessoryController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Accessory;
 use Illuminate\Http\Request;
+use Mockery\Exception;
 
 class AccessoryController extends Controller
 {
-    //
+    public function addOrUpdateAccessory(Request $request, $id){
+        $accessory = Accessory::where('id', $id)->first();
+        if ($accessory == NULL){
+            $accessory = new Accessory();
+        }
+        try{
+            $accessory->name = $request->name;
+            $accessory->description = $request->description;
+            $accessory->price = $request->price;
+            $accessory->save();
+        }
+        catch (Exception $e){
+            return view();
+        }
+        return view();
+    }
+
+    public function deleteAccessory($id){
+        $accessory = Accessory::where('id', $id)->firstOrFail();
+        $accessory->delete();
+        return view();
+    }
 }
