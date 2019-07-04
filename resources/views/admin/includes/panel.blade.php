26c5161e0244fd177b09a34416b73d30dd98db3e
Chayan Datta <sonicxxx7@gmail.com>
2019-07-04 13:18:03 +0530

    Admin panel settings edit

--- resources/views/admin/includes/panel.blade.php
+++ resources/views/admin/includes/panel.blade.php
@@ -18,22 +18,22 @@
                 <li class="menu-item-has-children dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Cars</a>
                     <ul class="sub-menu children dropdown-menu">
-                        <li><i class="fa fa-id-badge"></i><a href="/addProduct">Add</a></li>
-                        <li><i class="fa fa-exclamation-triangle"></i><a href="/product">Delete</a></li>
+                        <li><i class="fa fa-id-badge"></i><a href="/addCar">Add</a></li>
+                        <li><i class="fa fa-exclamation-triangle"></i><a href="/car">Delete</a></li>
                     </ul>
                 </li>
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Companies</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Accessories</a>
                     <ul class="sub-menu children dropdown-menu">
-                        <li><i class="menu-icon fa fa-fort-awesome"></i><a href="/addCompany">Add</a></li>
-                        <li><i class="fa fa-exclamation-triangle"></i><a href="/company">Delete</a></li>
+                        <li><i class="menu-icon fa fa-fort-awesome"></i><a href="/addAccessory">Add</a></li>
+                        <li><i class="fa fa-exclamation-triangle"></i><a href="/accessory">Delete</a></li>
                     </ul>
                 </li>
                 <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Categories</a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Modifications</a>
                     <ul class="sub-menu children dropdown-menu">
-                        <li><i class="menu-icon fa fa-balance-scale"></i><a href="/addCategory">Add</a></li>
-                        <li><i class="menu-icon fa fa-exclamation-triangle"></i><a href="/category">Delete</a></li>
+                        <li><i class="menu-icon fa fa-balance-scale"></i><a href="/addModification">Add</a></li>
+                        <li><i class="menu-icon fa fa-exclamation-triangle"></i><a href="/modification">Delete</a></li>
                     </ul>
                 </li>
 
@@ -42,27 +42,27 @@
                 <li class="menu-item-has-children dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Media Highlights</a>
                     <ul class="sub-menu children dropdown-menu">
-                        <li><i class="menu-icon fa fa-fort-awesome"></i><a href="/addCompany">Add</a></li>
-                        <li><i class="fa fa-exclamation-triangle"></i><a href="/company">Delete</a></li>
+                        <li><i class="menu-icon fa fa-fort-awesome"></i><a href="/addMedia">Add</a></li>
+                        <li><i class="fa fa-exclamation-triangle"></i><a href="/media">Delete</a></li>
                     </ul>
                 </li>
                 <li class="menu-item-has-children dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Gallery</a>
                     <ul class="sub-menu children dropdown-menu">
-                        <li><i class="menu-icon fa fa-balance-scale"></i><a href="/addCategory">Add</a></li>
-                        <li><i class="menu-icon fa fa-exclamation-triangle"></i><a href="/category">Delete</a></li>
+                        <li><i class="menu-icon fa fa-balance-scale"></i><a href="/addGallery">Add</a></li>
+                        <li><i class="menu-icon fa fa-exclamation-triangle"></i><a href="/gallery">Delete</a></li>
                     </ul>
                 </li>
                 <h3 class="menu-title">Reviews</h3><!-- /.menu-title -->
                 <li class="menu-item-has-children dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Reviews</a>
                     <ul class="sub-menu children dropdown-menu">
-                        <li><i class="menu-icon fa fa-balance-scale"></i><a href="/addCategory">Add</a></li>
-                        <li><i class="menu-icon fa fa-exclamation-triangle"></i><a href="/category">Delete</a></li>
+                        <li><i class="menu-icon fa fa-balance-scale"></i><a href="/addReview">Add</a></li>
+                        <li><i class="menu-icon fa fa-exclamation-triangle"></i><a href="/review">Delete</a></li>
                     </ul>
                 </li>
                 <li class="active">
-                    <a href="index.html"> <i class="menu-icon fa fa-sign-in"></i>Log Out </a>
+                    <a href="/logout"> <i class="menu-icon fa fa-sign-in"></i>Log Out </a>
                 </li>
             </ul>
         </div><!-- /.navbar-collapse -->
